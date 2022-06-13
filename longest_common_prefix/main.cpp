49105ea4edc9d8a51e125340b828559ba1bda05a
梅智皓 <meich19019@gmail.com>
2022-06-13 17:37:10 +0800

    Initial Commit

---
+++ longest_common_prefix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  longest_common_prefix
+//
+//  Created by 梅智皓 on 2022/6/13.
+//  Copyright © 2022年 梅智皓. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
